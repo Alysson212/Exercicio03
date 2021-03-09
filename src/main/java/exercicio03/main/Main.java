e0e5a15520cf97de44e74d3209f7947de35efc16
Alysson <alyssonhenrique720@gmail.com>
2021-03-09 20:07:11 -0300

    first commit

---
+++ src/main/java/exercicio03/main/Main.java
@@ -0,0 +1,34 @@
+package exercicio03.main;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Main app = new Main();
+		app.start();
+
+	}
+
+	private void start() {
+
+		System.out.print("Digite a idade: ");
+
+		Scanner scan = new Scanner(System.in);
+		int idade = scan.nextInt();
+
+		if (idade > 20) {
+			System.out.println("Idade maior que 20");
+		} else if (idade > 12 && idade < 17) {
+			System.out.println("Idade maior que 12 e menor que 17");
+		} else if (idade == 7 || idade < 5) {
+			System.out.println("Idade igual a 7 ou menor que 5");
+		} else {
+			System.out.println("Outras combinações");
+		}
+		
+
+	}
+
+}
